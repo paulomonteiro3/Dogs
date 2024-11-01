d02e5cd5c1d03db3cc0f614a9c89649f99db20b1
Paulo Monteiro <paulomonteirop22@gmail.com>
2024-11-01 11:56:02 -0300

    estrutura inicial criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-import Api from './api/Api';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+
+      </Routes>
+      <Footer />
+      </BrowserRouter>
     </div>
   )
 }
