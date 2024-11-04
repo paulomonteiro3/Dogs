345bf8ae187b7413a50e94f8ee542ead6fb77736
Paulo Monteiro <paulomonteirop22@gmail.com>
2024-11-04 14:58:09 -0300

    funcao de context criada. header e login form atualizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
-import Login from './Components/Login/Login';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/login/*' element={<Login />}></Route>
-
-      </Routes>
-      <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login/*" element={<Login />}></Route>
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
