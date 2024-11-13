a3a86b3a51e8a8e2079d1e686608281400f872d4
Paulo Monteiro <paulomonteirop22@gmail.com>
2024-11-13 00:43:54 -0300

    rota de perfil de usuario criada

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { UserStorage } from "./UserContext";
 import User from "./Components/User/User";
 import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
             <Route
               path="conta/*"
               element={
