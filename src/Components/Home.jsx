e618ef31f493e14c610010927e351f3752dc6380
Paulo Monteiro <paulomonteirop22@gmail.com>
2024-11-19 15:34:46 -0300

    correcao de alguns bugs gerais

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Feed from './Feed/Feed';
-import Head from './Helper/Head'
+import Head from './Helper/Head';
 
 const Home = () => {
   return (
     <section className="container mainContainer">
-      <Head title="Fotos" description="Home do site Dogs, com o feed de fotos." />
+      <Head
+        title="Fotos"
+        description="Home do site Dogs, com o feed de fotos."
+      />
       <Feed />
     </section>
   );
